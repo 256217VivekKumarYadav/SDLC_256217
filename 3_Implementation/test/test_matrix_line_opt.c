7f5c65f7e5d32920ab55867d1c19237c60f893d3
Vivek Kumar Yadav <84492890+99004434-VivekKumar@users.noreply.github.com>
2021-05-23 19:45:59 +0530

    Create test_matrix_line_opt.c

---
+++ 3_Implementation/test/test_matrix_line_opt.c
@@ -0,0 +1,46 @@
+/**
+ * @file test_up_down.c
+ * @author Praveen Kumar G - 99004437(praveen.kumar5@ltts.com)
+ * @brief Unit Testing for Matrix Line input option
+ * @version 0.1
+ * @date 2021-05-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include "unity_internals.h"
+#include "unity.h"
+#include "matrix.h"
+
+/* Required by the unity test framework */
+void setUp(){}
+
+/* Required by the unity test framework */
+void tearDown(){}
+
+
+int test_matrix_line_opt(t_struct *s, int key, int *i)
+{
+    TEST_ASSERT_EQUAL(2, pack_nb( 0 ,2, 1));
+    TEST_ASSERT_EQUAL(3, pack_nb( 0 ,3, 1));
+    TEST_ASSERT_EQUAL(12,pack_nb( 1 ,2, 2));
+    TEST_ASSERT_EQUAL(" ",pack_nb( 1 ,0x107 ,1));
+    TEST_ASSERT_EQUAL(" ",pack_nb( 10 ,0x107 ,2));
+    
+    
+    TEST_ASSERT_EQUAL(0,0);
+}
+int main()
+{
+/* Initiate the Unity Test Framework */
+  UNITY_BEGIN();
+
+/* Run Test functions */
+  //RUN_TEST(test_mainmenu);
+  RUN_TEST(test_matrix_line_opt);
+  
+
+  /* Close the Unity Test Framework */
+  return UNITY_END();
+}
