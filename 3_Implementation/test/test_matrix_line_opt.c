4ff535a792bdc0c6df45409955b689697a327308
99004437-Praveen <84492987+99004437-Praveen@users.noreply.github.com>
2021-05-24 23:47:44 +0530

    Update test_matrix_line_opt.c

--- 3_Implementation/test/test_matrix_line_opt.c
+++ 3_Implementation/test/test_matrix_line_opt.c
@@ -1,63 +1,48 @@
-
 /**
- * @file main.c
- * @author Praveen Kumar G - 99004437(praveen.kumar5@ltts.com)
- * @brief function definition for opening page of game and background
+ * @file test_playermode.c
+ * @author Vivek Kumar Yadav - 99004434(vivek.yadav@ltts.com)
+ * @brief unit testing for each player mode options
  * @version 0.1
- * @date 2021-05-23
+ * @date 2021-05-22
  * 
  * @copyright Copyright (c) 2021
  * 
  */
-
-
-
 #include "matrix.h"
+#include "unity.h"
 
-int	start_curse()
-{
-  if (initscr() == NULL)
-    return (str_put("Couldn't initscr.\n", 2));
-  start_color();
-  init_pair(1, COLOR_WHITE, COLOR_BLUE);
-  init_pair(2, COLOR_YELLOW, COLOR_BLUE);
-  init_pair(3, COLOR_YELLOW, COLOR_BLACK);
-  curs_set(0);
-  if (noecho() == ERR)
-    return (str_put("Couldn't noecho.\n", 2));
-  if (keypad(stdscr, TRUE) == ERR)
-    return (str_put("Couldn't keypad.\n", 2));
-  return (0);
-}
-/**
- * @brief quits the game
- * 
- */
-void	quit(t_struct *s)
-{
-  if (s != NULL)
-    free(s);
-  endwin();
-  exit(0);
+/* Required by the unity test framework */
+void setUp(){}
+/* Required by the unity test framework */
+void tearDown(){}
+
+/* Prototypes for all the test functions */
+int test_start_curse();
+
+
+/* Write all the test functions */ 
+int start_curse() {
+  
+  TEST_ASSERT_EQUAL_INT(0, start_curse());
 }
 
-int		main()
+/* Start of the application test */
+int main()
 {
-  t_struct	*s;
-  WINDOW	*frame;
+/* Initiate the Unity Test Framework */
+  UNITY_BEGIN();
 
-  if (start_curse() == -1 || (s = malloc(sizeof(*s))) == NULL)
-    quit(s);
-  if ((frame = newwin(25, 51, 1, 3)) == NULL)
-    return (str_put("Couldn't create frame window.\n", 2));
-  if ((s->game = newwin(23, 49, 2, 4)) == NULL)
-    return (str_put("Couldn't create game window.\n", 2));
-  wbkgd(frame, COLOR_PAIR(3));
-  wbkgd(s->game, COLOR_PAIR(1));
-  wborder(frame, '+', '+', '+', '+', '+', '+', '+', '+');
-  wrefresh(stdscr);
-  wrefresh(frame);
-  player_mode_opt(s, 0);
-  quit(s);
-  return (0);
+/* Run Test functions */
+  //RUN_TEST(test_mainmenu);
+  RUN_TEST(test_start_curse);
+  //RUN_TEST(test_Washclothes);
+  //RUN_TEST(test_WashMenu);
+  //RUN_TEST(test_Soak);
+  //RUN_TEST(test_getTimesoak);
+  //RUN_TEST(test_Temperature);
+  //RUN_TEST(test_viewlog);
+  //RUN_TEST(test_processlog);
+
+  /* Close the Unity Test Framework */
+  return UNITY_END();
 }
